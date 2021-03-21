5bd8a980f9c4ee55f5cfce611fbc2776af8a9d13
Arthur Han <arthurhan269@gmail.com>
2021-03-21 13:53:42 -0300

    Updated priorityQueue

--- PriorityQueue/main.c
+++ PriorityQueue/main.c
@@ -1,42 +1,35 @@
-//testeFila.c
-
 #include<stdio.h>
 #include<stdlib.h>
-#include "FilaPrioridade.h"
+#include "priorityQueue.h"
 
 int main(){
-	// Desenvolva os testes para verificar se a função implementada 
-  //está funcionando como esperado
-  FilaPrioridade* f = filaP_criar();                     
-  filaP_inserir(f, 10);                        
-  filaP_inserir(f, 9);                         
-  filaP_inserir(f, 4);
-  filaP_inserir(f, 7);       
-  filaP_inserir(f, 8);                  
-  filaP_inserir(f, 3);                         
-  filaP_inserir(f, 2);                         
-  filaP_inserir(f, 6);                         
-  filaP_inserir(f, 1);                         
-  filaP_inserir(f, 5);                         
-  filaP_imprimir(f);
-  printf("fila clonada:\n");
-  FilaPrioridade* fc = filaP_clone(f);
-  filaP_imprimir(fc);       
-  printf("fila 2:\n");    
-  TipoElemento v[6] = {3,6,5,1,7};        
-  FilaPrioridade* f2 = filaP_criar();
-  filaP_inserir(f2, 10);                        
-  filaP_inserir(f2, 9);                         
-  filaP_inserir(f2, 4);
-  filaP_inserir(f2, 7);       
-  filaP_inserir(f2, 8); 
-  filaP_inserirTodos(f2, v, 6);
-  filaP_imprimir(f2);   
 
-  printf("%d elementos na fila 1\n", filaP_tamanho(f));
-  int removido;
-  while(!filaP_vazia(f)){
-    filaP_remover(f, &removido);
-    printf("%d ", removido);     // saída esperada: 10 9 8 7 6 5 4 3 2 1 
-  }
+  priorityQueue* f = createQueue();                     
+  insertInQueue(f, 10);                        
+  insertInQueue(f, 1);                         
+  insertInQueue(f, 9);                         
+  insertInQueue(f, 8);                  
+  insertInQueue(f, 7);       
+  insertInQueue(f, 6);                         
+  insertInQueue(f, 5);                         
+  insertInQueue(f, 4);
+  insertInQueue(f, 3);                         
+  insertInQueue(f, 2);                         
+  print(f);
+  printf("fila clonada:\n");  
+  priorityQueue* fc = cloneQueue(f);
+  print(fc);       
+  printf("fila 2:\n");    
+  DataType v[6] = {3,6,5,1,7};        
+  priorityQueue* f2 = createQueue();
+  insertInQueue(f2, 10);                        
+  insertInQueue(f2, 9);                         
+  insertInQueue(f2, 4);
+  insertInQueue(f2, 7);       
+  insertInQueue(f2, 8); 
+  insertAllElements(f2, v, 6);
+  print(f2);   
+
+  printf("%d elementos na fila 1\n", getSize(f));
+
 }
