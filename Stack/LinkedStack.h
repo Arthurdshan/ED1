2c20cc0c7825e762749c4884acc1fce8e49a0d25
Arthur Han <arthurhan269@gmail.com>
2021-03-21 03:30:47 -0300

    updated stack and folder names

---
+++ Stack/LinkedStack.h
@@ -0,0 +1,138 @@
+#pragma once 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef enum boolean{FALSE, TRUE} Boolean;
+typedef int DataType;
+
+typedef struct node{
+	DataType value;
+	struct node *next;
+}Node;
+
+typedef struct {
+	Node *top;
+	int size;
+}Stack;
+
+Stack* createStack();
+void destroyStack(Stack* p);
+Boolean pushElement(Stack* p, DataType value);
+Boolean popElement(Stack* p, DataType* exit);
+Boolean topElement(Stack* p, DataType* exit);
+void print(Stack* p);
+int getSize(Stack* p);
+Stack* cloneStack(Stack* p);
+void revertStack(Stack* p);
+Boolean pushAllElements(Stack* p, DataType* vector, int size);
+
+Stack* createStack(){
+  
+  Stack* p = (Stack*) malloc(sizeof(Stack));
+  p->size = 0;
+  return p;
+}
+
+void destroyStack(Stack* p){
+
+  if(p == NULL) return;
+
+  Node* aux;
+  while(p->top != NULL){
+    aux = p->top;
+    p->top = p->top->next;
+    free(aux);
+  }
+  free(p);
+}
+
+Boolean pushElement(Stack* p, DataType value){
+  
+  if(p == NULL) return FALSE;
+  
+  Node* node = (Node*) malloc(sizeof(Node));
+  if(node == NULL) return FALSE;
+  node->value = value;
+
+  node->next = p->top;
+  p->size++;
+  p->top = node;
+  return TRUE;
+}
+
+Boolean popElement(Stack* p, DataType* exit){
+  
+  if(p == NULL || p->top == NULL) return FALSE;
+
+  *exit = p->top->value;
+  Node* aux = p->top;
+  p->top = aux->next;
+  p->size--; 
+  free(aux);
+  return TRUE;
+}
+
+void print(Stack* p){
+  
+  Node* teste = p->top;
+  while(teste != NULL){
+    printf("%d -> ", teste->value);
+    teste = teste->next;
+  }
+  printf("NULL\n");
+}
+
+Boolean topElement(Stack* p, DataType* exit){
+  
+  if (p == NULL || p->top == NULL) return FALSE;
+
+  *exit = p->top->value;
+  return TRUE; 
+}
+
+int getSize(Stack* p){
+  return p->size;
+}
+
+Stack* cloneStack(Stack* p){
+  
+  Stack* clone = (Stack*) malloc(sizeof(Stack));
+  Node* caminho = p->top;
+  Node* temp;
+  for(int i = 0; i < p->size; i++){
+    Node* value = (Node*) malloc(sizeof(Node));
+    value->value = caminho->value;
+      
+    if(i == 0) clone->top = value;
+    else temp->next = value;
+      
+    temp = value;
+    caminho = caminho->next;
+  }
+  return clone;
+}
+
+void revertStack(Stack* p){
+  
+  Stack* p1 = createStack();
+  Node* aux = p->top;
+  while(aux != NULL){
+    pushElement(p1,aux->value);
+    aux = aux->next;
+  }
+  p1->size = p->size;
+  p->top = p1->top;
+}
+
+Boolean pushAllElements(Stack* p, DataType* vetor, int size){
+
+  if (p == NULL) return FALSE;
+
+  for(int i = 0; i < size; i++){
+    int aux = vetor[i];
+    pushElement(p, aux);
+    p->size++;
+  }
+  return TRUE;
+}
---
+++ Stack/StaticStack.h
@@ -0,0 +1,105 @@
+#pragma once 
+
+#include <stdio.h>
+#include <stdlib.h>
+#define N 5
+
+typedef enum boolean{FALSE, TRUE} Boolean;
+typedef int DataType;
+
+typedef struct {
+	DataType* vector;
+	int length;
+	int size;
+}Stack;
+
+Stack* createStack();
+void destroyStack(Stack* p);
+Boolean pushElement(Stack* p, DataType value);
+Boolean popElement(Stack* p, DataType* saida);
+Boolean topElement(Stack* p, DataType* saida);
+void print(Stack* p);
+int getSize(Stack* p);
+Stack* cloneStack(Stack* p);
+void revertStack(Stack* p);
+Boolean pushAllElements(Stack* p, DataType* vector, int lengthvector);
+
+Stack* createStack(){
+  Stack* p1= (Stack*) malloc(sizeof(Stack));
+  p1->vector = (DataType*) calloc(N, sizeof(DataType)); 
+  p1->size = 0;
+  p1->length = N;
+  return p1;
+}
+
+void createStack(Stack* p){
+  free(p->vector);
+  free(p);
+}
+
+Boolean pushElement(Stack* p, DataType value){
+  if(p == NULL) return FALSE;
+  if(p->size == p->length) return FALSE;
+  p->vector[p->size] = value;
+  p->size++;
+  return TRUE;
+}
+
+Boolean popElement(Stack* p, DataType* saida){
+  if(p == NULL || p->size == 0) return FALSE;
+
+  *saida = p->vector[p->size - 1];
+  p->size--;
+  return TRUE;
+  }
+
+Boolean topElement(Stack* p, DataType* saida){
+  if (p == NULL) return FALSE;
+
+  saida = &p->vector[p->size];
+  return TRUE; 
+}
+
+void print(Stack *p){
+  if (p == NULL) return;
+  
+  for(int i = 0; i < 5; i++){
+    if(i == p->size-1) printf("%d\n", p->vector[i]);
+    else printf("%d -> ", p->vector[i]);
+  }
+}
+
+int getSize(Stack* p){
+  return p->size;
+}
+
+Stack* cloneStack(Stack* p){
+  Stack* p1= createStack();
+  p1->length = p->length;
+  p1->size = p->size;
+  for(int i = 0; i < p->size; i++){
+    p1->vector[i] = p->vector[i];
+  }
+  return p1;
+}
+
+void revertStack(Stack* p){
+  
+  if(p == NULL) return;
+
+  DataType* inverso = (DataType*) calloc(p->length, sizeof(DataType));
+  for(int i = p->size - 1, j = 0; i >= 0; i--, j++){
+    inverso[j] = p->vector[i];
+  }
+  free(p->vector);
+  p->vector = inverso;
+}
+
+Boolean pushAllElements(Stack* p, DataType* vector, int lengthvector){
+  if (p == NULL|| lengthvector <= 0) return FALSE;
+
+  for(int i = 0; i < lengthvector; i++){
+    pushElement(p, vector[i]);
+  }
+  return TRUE;
+}
